e2b92d94dab83c5be738388215a3a083b3bd30cd
Lemonchelo16 <124084741+Lemonchelo16@users.noreply.github.com>
2023-05-16 13:24:37 -0300

    Correccion relaciones y publicaciones Testeos.hs

--- Testeos.hs
+++ Testeos.hs
@@ -12,23 +12,23 @@ allTests = test [
     ]
 
 testEj1 = test [
-    "nombresDeUsuarios 1 (comun)" ~: nombresDeUsuarios redA ~?= ["Juli", "Santi", "Luca", "Diego"],
+    "nombresDeUsuarios 1 (comun)" ~: nombresDeUsuarios redB ~?= ["Juli", "Santi", "Luca", "Diego"],
     "nombresDeUsuarios 2 (sin usuarios)" ~: nombresDeUsuarios redVacia ~?= [],
-    "nombresDeUsuarios 3 (repetidos)" ~: nombresDeUsuarios redRepetidos ~?= ["Juli", "Santi", "Luca", "Diego"],
+    "nombresDeUsuarios 3 (repetidos)" ~: nombresDeUsuarios redA ~?= ["Juli", "Santi", "Luca", "Diego"],
     "nombresDeUsuarios 4 (1 usuario)" ~: nombresDeUsuarios redUnUsuario ~?= ["Juli"]
     ]
 
 testEj6 = test [
     "publicacionesDe 1 " ~: publicacionesDe redA usuario1 ~?= [publicacion1_1, publicacion1_3],
     "publicacionesDe 2 (red sin publi)" ~: publicacionesDe redUsuarios usuario2 ~?= [],
-    "publicacionesDe 3 (usuario sin publi) " ~: publicacionesDe redA  usuario2 ~?= []
+    "publicacionesDe 3 (usuario sin publi) " ~: publicacionesDe redB  usuario2 ~?= []
     
     ]
 
 testEj7 = test [
-    "publicacionesQueLeGustanA 1 " ~: publicacionesQueLeGustanA redA usuario2 ~?= [publicacion1_1, publicacion3_1],
-    "publicacionesQueLeGustanA 2 (ningun like) " ~: publicacionesQueLeGustanA redA usuario3 ~?= [],
-    "publicacionesQueLeGustanA 3 (red sin publi) " ~: publicacionesQueLeGustanA redRepetidos usuario4 ~?= []
+    "publicacionesQueLeGustanA 1 " ~: publicacionesQueLeGustanA redA usuario2 ~?= [publicacion1_1, publicacion1_3, publicacion3_1],
+    "publicacionesQueLeGustanA 2 (ningun like) " ~: publicacionesQueLeGustanA redB usuario3 ~?= [],
+    "publicacionesQueLeGustanA 3 (red sin publi) " ~: publicacionesQueLeGustanA redA usuario4 ~?= []
 
     ]
 
@@ -43,16 +43,16 @@ usuario5 = (5, "Diego")
 --PUBLIACIONES
 publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2])
 publicacion1_2 = (usuario1, "Lorem ipsum dolor sit amet", [usuario5, usuario4])
-publicacion1_3 = (usuario1, "Vivamus et risus at purus pharetra", [usuario4, usuario2])
-publicacion1_4 = (usuario1, "Maecenas pellentesque nulla vel", [usuario5, usuario3])
+publicacion1_3 = (usuario1, "Vivamus et risus at purus pharetra", [usuario4, usuario2, usuario5, usuario3])
+publicacion1_4 = (usuario1, "Maecenas pellentesque nulla vel", [usuario3])
 
 publicacion2_1 = (usuario2, "Phasellus eget tellus eu", [usuario3, usuario4])
 publicacion2_2 = (usuario2, "Morbi nec orci", [usuario1, usuario3])
 publicacion2_3 = (usuario2, "Nullam sed risus et", [usuario4, usuario1])
-publicacion2_4 = (usuario2, "Sed viverra est at augu", [usuario3, usuario5])
+publicacion2_4 = (usuario2, "Sed viverra est at augu", [usuario3, usuario5, usuario2])
 
 publicacion3_1 = (usuario3, "Morbi dapibus neque", [usuario5, usuario2])
-publicacion3_2 = (usuario3, "Duis ac neque quis", [usuario1, usuario5])
+publicacion3_2 = (usuario3, "Duis ac neque quis", [usuario1, usuario5, usuario4])
 publicacion3_3 = (usuario3, "Nullam eget quam", [usuario1, usuario5])
 publicacion3_4 = (usuario3, "Leo id eleifend tempus", [usuario2, usuario4]) 
 
@@ -69,10 +69,30 @@ publicacion5_4 = (usuario5, "Este es mi primer post", [usuario3, usuario2])
 --RELACIONES
 relacion1_2 = (usuario1, usuario2)
 relacion1_3 = (usuario1, usuario3)
-relacion1_4 = (usuario4, usuario1) -- Notar que el orden en el que aparecen los usuarios es indistinto
-relacion2_3 = (usuario3, usuario2)
+relacion1_4 = (usuario1, usuario4)
+relacion1_5 = (usuario1, usuario5)
+
+relacion2_1 = (usuario2, usuario1)
+relacion2_3 = (usuario2, usuario3)
 relacion2_4 = (usuario2, usuario4)
-relacion3_4 = (usuario4, usuario3)
+relacion2_5 = (usuario2, usuario5)
+
+relacion3_1 = (usuario3, usuario1)
+relacion3_2 = (usuario3, usuario2)
+relacion3_4 = (usuario3, usuario4)
+relacion3_5 = (usuario3, usuario5)
+
+relacion4_1 = (usuario4, usuario1)
+relacion4_2 = (usuario4, usuario2)
+relacion4_3 = (usuario4, usuario3)
+relacion4_5 = (usuario4, usuario5)
+
+relacion5_1 = (usuario5, usuario1)
+relacion5_2 = (usuario5, usuario2)
+relacion5_3 = (usuario5, usuario3)
+relacion5_4 = (usuario5, usuario4)
+
+
 
 -- REDES
 redVacia = ([],[],[])
@@ -83,12 +103,12 @@ redUsuarios = (usuariosA,[],[])
 
 redA = (usuariosA, relacionesA, publicacionesA)
 -- redA
-usuariosA = [usuario1, usuario2, usuario3, usuario4]
+usuariosA = [usuario1, usuario2, usuario3, usuario4, usuario5]
 publicacionesA = [publicacion1_1, publicacion1_3, publicacion3_1, publicacion3_2, publicacion4_3]
-relacionesA = [relacion1_2, relacion1_3, relacion2_3, relacion3_4]
+relacionesA = [relacion1_2, relacion1_3, relacion4_1, relacion5_1, relacion3_5, relacion2_3, usuario4_3]
 
 -- redRepetidos
-usuariosRepetidos = [usuario1, usuario2, usuario3, usuario4, usuario5]
+usuariosB = [usuario1, usuario2, usuario3, usuario4]
 redRepetidos = (usuariosRepetidos, [], [])
 
 
