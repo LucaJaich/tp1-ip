11afea526cff72e5565b18aee0136a8d68002904
Lemonchelo16 <124084741+Lemonchelo16@users.noreply.github.com>
2023-05-16 14:53:23 -0300

    Correccion de testeos y funciones 16/05

--- SolucionDiego.hs
+++ SolucionDiego.hs
@@ -1,13 +1,11 @@
--- Completar con los datos del grupo
---
+module SolucionDiego where
+
+
 -- Nombre de Grupo: xx
 -- Integrante 1: Nombre Apellido, email, LU
 -- Integrante 2: Nombre Apellido, email, LU
 -- Integrante 3: Nombre Apellido, email, LU
 -- Integrante 4: Nombre Apellido, email, LU
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Redundant bracket" #-}
 
 type Usuario = (Integer, String) -- (id, nombre)
 type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
@@ -39,12 +37,26 @@ likesDePublicacion (_, _, us) = us
 
 -- Ejercicios
 
-nombresDeUsuarios :: RedSocial -> [String]
-nombresDeUsuarios red = proyectarNombres (usuarios red) []
+--pertenece recibe un elemento y una lista y devuelve True si el elemento esta incluido en la lista
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece _ [] = False
+pertenece n (x:xs) | n == x = True
+                   | otherwise = pertenece n xs
 
-proyectarNombres :: [Usuario] -> [String] -> [String]
-proyectarNombres usuarios nombres | null usuarios = nombres
-                                  | otherwise = proyectarNombres (tail usuarios) (nombres ++ [nombreDeUsuario(head usuarios)])
+--sinRepetidos recibe una lista y te la devuelve sin ningun repetidos
+sinRepetidos :: (Eq t) => [t] -> [t]
+sinRepetidos [] = []
+sinRepetidos (x:xs) | pertenece x xs = (sinRepetidos xs)
+                    | otherwise = x : (sinRepetidos xs)
+
+--listaDeNombres recibe una lista de usuarios y devuelve una lista con esos nombres sin los Ids
+listaDeNombres :: [Usuario] -> [String]
+listaDeNombres [] = []
+listaDeNombres (u:us) = (nombreDeUsuario u) : (listaDeNombres us) 
+
+--nombresDeUsuarios recibe una red social y devuelve los nombres de usuarios 
+nombresDeUsuarios :: RedSocial -> [String]
+nombresDeUsuarios rs = sinRepetidos (listaDeNombres (usuarios rs))
 
 -- describir qué hace la función: .....
 amigosDe :: RedSocial -> Usuario -> [Usuario]
@@ -93,71 +105,36 @@ publicacionesQueLeGustanA red usu | null (publicaciones red) = []
                                         publicacionLike = (head(publicaciones red))
                                         recuLikePubli = (usuarios red, relaciones red, tail(publicaciones red))
 
-pertenece :: (Eq t) => t -> [t] -> Bool
-pertenece x xs | null xs = False
-               | x == head xs = True
-               | x /= head xs = pertenece x (tail xs)
-               | otherwise = False
 
--- describir qué hace la función: .....
+-- describir qué hace la función: Es verdadero si los dos usuarios le dieron me gusta a las mismas publicaciones
 lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
-lesGustanLasMismasPublicaciones red usu1 usu2 | mismosElementos (publicacionesQueLeGustanA red usu1) (publicacionesQueLeGustanA red usu2) = True
-                                              | otherwise = False
+lesGustanLasMismasPublicaciones red usuario1 usuario2 | mismosElementos (publicacionesQueLeGustanA red usuario1) (publicacionesQueLeGustanA red usuario2) = True
+                                                      | otherwise = False
 
-mismosElementos :: (Eq t) => [t] -> [t] -> Bool
-mismosElementos ls1 ls2 = listaPerteneceLista ls1 ls2 && listaPerteneceLista ls2 ls1
+mismosElementos :: [Publicacion] -> [Publicacion] -> Bool
+mismosElementos publicaciones_usuario1 publicaciones_usuario2 | estaIncluida publicaciones_usuario1 publicaciones_usuario2, estaIncluida publicaciones_usuario2 publicaciones_usuario1 = True
+                                                              | otherwise = False
 
-listaPerteneceLista :: (Eq t) => [t] -> [t] -> Bool
-listaPerteneceLista [] _ = True
-listaPerteneceLista (x:xs) ls = (pertenece x ls) && listaPerteneceLista xs ls
+estaIncluida :: [Publicacion] -> [Publicacion] -> Bool
+estaIncluida publicaciones_usuario1 publicaciones_usuario2 | publicaciones_usuario1 == [] = True
+                                                           | pertenece (head publicaciones_usuario1) publicaciones_usuario2 = estaIncluida (tail publicaciones_usuario1) publicaciones_usuario2
+                                                           | otherwise = False
 
 
--- describir qué hace la función: .....
+
+-- describir qué hace la función: Es verdadero si existe un usuario que le dio me gusta en todas sus publicaciones
 tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
-tieneUnSeguidorFiel red usu = undefined
+tieneUnSeguidorFiel red usuario | publicacionesDe red usuario == [] = False
+                                | otherwise = veoMeGustas red usuario (publicacionesDe red usuario) (amigosDe red usuario)
+
+veoMeGustas :: RedSocial -> Usuario -> [Publicacion] -> [Usuario] -> Bool
+veoMeGustas red usuario publicaciones_del_usuario amigos | amigos == [] = False
+                                                         | publicaciones_del_usuario == [] = True
+                                                         | pertenece (head amigos) (likesDePublicacion (head publicaciones_del_usuario)) = veoMeGustas red usuario (tail publicaciones_del_usuario) amigos
+                                                         | otherwise = veoMeGustas red usuario (publicacionesDe red usuario) (tail amigos)
 
 -- describir qué hace la función: .....
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
 existeSecuenciaDeAmigos red usu1 usu2 = undefined
 
 
-usuario1 = (1, "Juan")
-usuario2 = (2, "Natalia")
-usuario3 = (3, "Pedro")
-usuario4 = (4, "Mariela")
-usuario5 = (5, "Natalia")
-
-relacion1_2 = (usuario1, usuario2)
-relacion1_3 = (usuario1, usuario3)
-relacion1_4 = (usuario4, usuario1) -- Notar que el orden en el que aparecen los usuarios es indistinto
-relacion2_3 = (usuario3, usuario2)
-relacion2_4 = (usuario2, usuario4)
-relacion3_4 = (usuario4, usuario3)
-
-publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4])
-publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
-publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])
-publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
-publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
-
-publicacion2_1 = (usuario2, "Hello World", [usuario4])
-publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
-
-publicacion3_1 = (usuario3, "Lorem Ipsum", [])
-publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])
-publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])
-
-publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
-publicacion4_2 = (usuario4, "I am Bob", [])
-publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
-
-
-usuariosA = [usuario1, usuario2, usuario3, usuario4]
-relacionesA = [relacion1_2, relacion1_4, relacion2_3, relacion2_4, relacion3_4]
-publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2]
-redA = (usuariosA, relacionesA, publicacionesA)
-
-usuariosB = [usuario1, usuario2, usuario3, usuario5]
-relacionesB = [relacion1_2, relacion2_3]
-publicacionesB = [publicacion1_3, publicacion1_4, publicacion1_5, publicacion3_1, publicacion3_2, publicacion3_3]
-redB = (usuariosB, relacionesB, publicacionesB)
--- Testeos.hs
+++ Testeos.hs
@@ -7,7 +7,10 @@ run = runTestTT allTests
 
 allTests = test [
     "nombresDeUsuarios" ~: testEj1,
-    "publicacionesDe" ~: testEj6
+    "publicacionesDe" ~: testEj6,
+    "publicacionesQueLeGustanA" ~: testEj7,
+    "lesGustanLasMismasPublicaciones" ~: testEj8,
+    "tieneUnSeguidorFiel" ~: testEj9
 
     ]
 
@@ -21,16 +24,59 @@ testEj1 = test [
 testEj6 = test [
     "publicacionesDe 1 " ~: publicacionesDe redA usuario1 ~?= [publicacion1_1, publicacion1_3],
     "publicacionesDe 2 (red sin publi)" ~: publicacionesDe redUsuarios usuario2 ~?= [],
-    "publicacionesDe 3 (usuario sin publi) " ~: publicacionesDe redB  usuario2 ~?= []
+    "publicacionesDe 3 (usuario sin publi) " ~: publicacionesDe redB  usuario2 ~?= [],
+    "publicacionesDe 4 (un usuario)" ~: publicacionesDe redUnUsuario usuario1 ~?= [] 
     
     ]
 
 testEj7 = test [
     "publicacionesQueLeGustanA 1 " ~: publicacionesQueLeGustanA redA usuario2 ~?= [publicacion1_1, publicacion1_3, publicacion3_1],
     "publicacionesQueLeGustanA 2 (ningun like) " ~: publicacionesQueLeGustanA redB usuario3 ~?= [],
-    "publicacionesQueLeGustanA 3 (red sin publi) " ~: publicacionesQueLeGustanA redA usuario4 ~?= []
+    "publicacionesQueLeGustanA 3 (red sin publi) " ~: publicacionesQueLeGustanA redB usuario4 ~?= []
 
     ]
+
+testEj8 = test [
+    "lesGustanLasMismasPublicaciones 1 (caso verdadero)" ~: lesGustanLasMismasPublicaciones redC usuario4 usuario5 ~?= True,
+    "lesGustanLasMismasPublicaciones 2 (caso falso)" ~: lesGustanLasMismasPublicaciones redA usuario3 usuario1 ~?= False,
+    "lesGustanLasMismasPublicaciones 3 (caso falso y solo usuarios)" ~: lesGustanLasMismasPublicaciones redUsuarios usuario4 usuario2 ~?= True 
+    
+    ]
+
+testEj9 = test [
+    "tieneUnSeguidorFiel 1 (caso verdadero)" ~: tieneUnSeguidorFiel redA usuario3 ~?= True, --usuario5 seguidor fiel
+    "tieneUnSeguidorFiel 2 (caso falso)" ~: tieneUnSeguidorFiel redA usuario2 ~?= False, 
+    "tieneUnSeguidorFiel 5 (caso sin likes)" ~: tieneUnSeguidorFiel redSinLikes usuario1 ~?= False
+
+    ]
+
+-- REDES
+redVacia = ([],[],[])
+
+redUnUsuario = ([usuario1], [], [])
+
+redUsuario1Publi = ([usuario1], [], [publicacion1_UnicoUsuario])
+
+redUsuariosRela = (usuariosA, relacionesA, [])
+
+--redSinUs
+redUsuarios = (usuariosA,[],[])
+
+redA = (usuariosA, relacionesA, publicacionesA)
+-- redA
+usuariosA = [usuario1, usuario2, usuario3, usuario4, usuario5]
+publicacionesA = [publicacion1_1, publicacion1_3, publicacion2_2, publicacion2_4, publicacion3_1, publicacion3_2, publicacion4_3]
+relacionesA = [relacion1_2, relacion1_3, relacion4_1, relacion5_1, relacion3_5, relacion2_3, relacion4_3]
+
+-- redRepetidos
+usuariosB = [usuario1, usuario2, usuario3, usuario4]
+redB = (usuariosB, [], [])
+
+redC = (usuariosA, relacionesA, publicacionesC)
+publicacionesC = [publicacion1_3, publicacion3_2, publicacion3_4]
+
+redSinLikes = (usuariosA, relacionesA, publicacionesSinLikes)
+publicacionesSinLikes = [publicacion1_5, publicacion2_5,publicacion3_5,publicacion4_5, publicacion5_5]
 
 
 -- USUARIOS
@@ -45,26 +91,33 @@ publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2])
 publicacion1_2 = (usuario1, "Lorem ipsum dolor sit amet", [usuario5, usuario4])
 publicacion1_3 = (usuario1, "Vivamus et risus at purus pharetra", [usuario4, usuario2, usuario5, usuario3])
 publicacion1_4 = (usuario1, "Maecenas pellentesque nulla vel", [usuario3])
+publicacion1_5 = (usuario1, "Batata y queso sin nulla vel", [])
+publicacion1_UnicoUsuario = (usuario1, "Estoy en soledad", [])
+
 
 publicacion2_1 = (usuario2, "Phasellus eget tellus eu", [usuario3, usuario4])
 publicacion2_2 = (usuario2, "Morbi nec orci", [usuario1, usuario3])
-publicacion2_3 = (usuario2, "Nullam sed risus et", [usuario4, usuario1])
-publicacion2_4 = (usuario2, "Sed viverra est at augu", [usuario3, usuario5, usuario2])
+publicacion2_3 = (usuario2, "Nullam sed risus et", [usuario3, usuario5, usuario1])
+publicacion2_4 = (usuario2, "Sed viverra est at augu", [usuario4, usuario5])
+publicacion2_5 = (usuario2, "Morbi nec orci", [])
 
 publicacion3_1 = (usuario3, "Morbi dapibus neque", [usuario5, usuario2])
 publicacion3_2 = (usuario3, "Duis ac neque quis", [usuario1, usuario5, usuario4])
 publicacion3_3 = (usuario3, "Nullam eget quam", [usuario1, usuario5])
-publicacion3_4 = (usuario3, "Leo id eleifend tempus", [usuario2, usuario4]) 
+publicacion3_4 = (usuario3, "Leo id eleifend tempus", [usuario2, usuario4, usuario5, usuario3])
+publicacion3_5 = (usuario3, "Trato de que eget quam", [])
 
 publicacion4_1 = (usuario4, "Aliquam tincidunt", [usuario1, usuario3])
 publicacion4_2 = (usuario4, "Integer sed augue et", [usuario5, usuario2])
 publicacion4_3 = (usuario4, "Morbi vitae dui id turpis", [])
 publicacion4_4 = (usuario4, "Sed dapibus purus", [usuario5, usuario1])
+publicacion4_5 = (usuario4, "Sed flota purus", [])
 
 publicacion5_1 = (usuario5, "Quisque id ante", [usuario1, usuario2])
 publicacion5_2 = (usuario5, "Etiam sempe", [usuario1, usuario4])
 publicacion5_3 = (usuario5, "Mauris eget ex a", [usuario3, usuario1])
 publicacion5_4 = (usuario5, "Este es mi primer post", [usuario3, usuario2])
+publicacion5_5 = (usuario5, "Este seria igual mientras primer post", [])
 
 --RELACIONES
 relacion1_2 = (usuario1, usuario2)
@@ -94,21 +147,5 @@ relacion5_4 = (usuario5, usuario4)
 
 
 
--- REDES
-redVacia = ([],[],[])
-redUnUsuario = ([usuario1], [], [])
-
---redSinUs
-redUsuarios = (usuariosA,[],[])
-
-redA = (usuariosA, relacionesA, publicacionesA)
--- redA
-usuariosA = [usuario1, usuario2, usuario3, usuario4, usuario5]
-publicacionesA = [publicacion1_1, publicacion1_3, publicacion3_1, publicacion3_2, publicacion4_3]
-relacionesA = [relacion1_2, relacion1_3, relacion4_1, relacion5_1, relacion3_5, relacion2_3, usuario4_3]
-
--- redRepetidos
-usuariosB = [usuario1, usuario2, usuario3, usuario4]
-redRepetidos = (usuariosRepetidos, [], [])
 
 
